84ebf6b7f9558a4536e8fcbcefa06c6585001477
Yejin_Cho <yy991125@naver.com>
2019-11-21 20:06:22 +0900

    db내 접근 기능 생성

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,25 +4,35 @@ class UsersController < ApplicationController
   # POST /api/user/host
   def host
     @user = User.create!(user_params)
-    # self.user.randomCode = ([*('A'..'Z'),*('0'..'9')]-%w(0 1 I O)).sample(8).join
+    @user.randomCode = (0...8).map { (65 + rand(26)).chr }.join
+    puts(@user.randomCode)
+    @user.save
     json_response(@user, :created)
   end
 
 # POST api/user/invite
   def invite
     @user = User.create!(user_params)
-
-    @otherUser = User.find(params[:받아온 randomCode])
-    # user.partnerUUID = @otherUser.myUUID
+    @otherUser = User.find_by(randomCode: params[:randomCode])
+    @otherUser.partnerUUID = @user.myUUID
+    @otherUser.save
+    @user.save
     json_response(@user, :created)
   end
 
-# PUT api/user/update
-  def update
-    # 위치를 받아와서 update
-    # longtitude, latitude 업데이트
-    # 상대의 위치와 비교
-    # 50m 이내이면 return true
+# POST api/user/update
+  def updateGPS
+    @user = User.find_by(myUUID: params[:myUUID])
+    # 여기까지 잘 옴.
+    # 여기 밑에 안되는 이유 모르겠음. 
+    @user.latitude = params[:latitude]
+    @user.longitude = params[:longitude]
+    @user.save
+
+    # 다른 유저 찾기
+    # @otherUser = User.where.not(myUUID: @user.myUUID).or(User.where(randomCode: @user.randomCode))
+    # json_response(@otherUser, :created)
+    json_response(@user, :created)
   end
 
   private
@@ -31,6 +41,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.permit(:myUUID, :partnerUUID)
+    params.permit(:myUUID, :partnerUUID, :latitude, :longitude, :randomCode)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@
 Rails.application.routes.draw do
   post '/api/user/host', to: 'users#host'
   post '/api/user/invite', to: 'users#invite'
-  put '/api/user/update', to: 'users#update'
+  post '/api/user/updateGPS', to: 'users#updateGPS'
 end
