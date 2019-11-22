da56041df5cacef82be1c4685af3a50eec5f5fbc
Yejin_Cho <yy991125@naver.com>
2019-11-22 13:06:36 +0900

    db내 잘못된 접근 수정

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,16 +23,13 @@ class UsersController < ApplicationController
 # POST api/user/update
   def updateGPS
     @user = User.find_by(myUUID: params[:myUUID])
-    # 여기까지 잘 옴.
-    # 여기 밑에 안되는 이유 모르겠음. 
     @user.latitude = params[:latitude]
     @user.longitude = params[:longitude]
     @user.save
 
     # 다른 유저 찾기
-    # @otherUser = User.where.not(myUUID: @user.myUUID).or(User.where(randomCode: @user.randomCode))
-    # json_response(@otherUser, :created)
-    json_response(@user, :created)
+    @otherUser = User.where.not(myUUID: @user.myUUID).where(randomCode: @user.randomCode)[0]
+    json_response(@otherUser, :created)
   end
 
   private
